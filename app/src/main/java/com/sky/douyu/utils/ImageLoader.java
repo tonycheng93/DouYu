f24e8a2e37bacbe92f64bc0ea157edfa65c7f3a6
tonycheng93 <tonycheng93@outlook.com>
2016-10-13 14:07:32 +0800

    开启Glide网络缓存

--- app/src/main/java/com/sky/douyu/utils/ImageLoader.java
+++ app/src/main/java/com/sky/douyu/utils/ImageLoader.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 /**
  * Created by tonycheng on 2016/10/11.
@@ -14,6 +15,7 @@ public class ImageLoader {
     public static void display(Context context, String url, ImageView imageView) {
         Glide.with(context)
                 .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(imageView);
     }
 }
